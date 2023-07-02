07059b712fd6563d241128b686996fc1ff637855
Andrew <andrew.i.funke@gmail.com>
2023-07-02 13:54:27 -0700

    Functions: Continued

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(param = "roller-skate") {
+    return "This Saturday, I want to " + param + "!";
+}
+
+// function mondayWork(param = "go to the office") {
+//     return "This Monday, I will " + param + ".";
+// }
+
+const mondayWork = function (param = "go to the office") {
+    return "This Monday, I will " + param + ".";
+}
+mondayWork();
+
+function wrapAdjective(flair = "*") {
+    inner = function (param = "special") {
+        return "You are " + flair + param + flair + "!";
+    }
+    return inner;
+}
